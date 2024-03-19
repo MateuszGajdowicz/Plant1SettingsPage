17cf66bfcdaf9c92dc031b821463919b2639e891
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-19 20:40:03 +0100

    platsettings

---
+++ index.js
@@ -0,0 +1,6 @@
+let PlantName = document.getElementById('p1');
+
+function SetName(){
+    let PlantName = document.getElementById('p1');
+    let NewPlantName = window.prompt("Name your plant :)")
+    PlantName.textContent = NewPlantName;}
